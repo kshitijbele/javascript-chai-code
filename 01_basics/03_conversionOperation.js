01dedfc2bda1fd6e438ca41018da25100c6f9e00
kshitijbele <42614121+kshitijbele@users.noreply.github.com>
2023-11-14 08:33:10 +0000

    + operator operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -33,3 +33,28 @@ console.log(valueInNumber2)
 // "kshitij" => true
 // null => false
 // undefined => false
+
+//************************ Operations **************************//
+
+console.log(2**3)
+let str1 = "hello"
+let str2 = " kshitij"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2) //12
+console.log(1 + "2") //12
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2") //32
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
